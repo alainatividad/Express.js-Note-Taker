0b85bcf0c41a53dbf070fb2b384b514fea120c88
AN <99376802+alainatividad@users.noreply.github.com>
2022-05-04 21:55:14 +1000

    Added code of route to get the specific note

--- routes/notes.js
+++ routes/notes.js
@@ -12,4 +12,16 @@ notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+// GET Route for a specific tip
+notes.get("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json(`No note with ID ${noteId}`);
+    });
+});
 module.exports = notes;
